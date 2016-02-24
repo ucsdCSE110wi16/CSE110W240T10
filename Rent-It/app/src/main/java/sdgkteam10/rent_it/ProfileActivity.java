c6100c7ac6b16d15b19786fc109a0402638a854d
hendrickson02 <hendrickson02@gmail.com>
2016-02-24 12:15:40 -0800

    Declared the added TextViews in ProfileActivity.java. They    will all show "loading..." upon opening.

--- Rent-It/app/src/main/java/sdgkteam10/rent_it/ProfileActivity.java
+++ Rent-It/app/src/main/java/sdgkteam10/rent_it/ProfileActivity.java
@@ -1,11 +1,15 @@
 package sdgkteam10.rent_it;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,26 +17,45 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView userName;
     private TextView userEmail;
     private TextView userPhone;
-    private TextView address1;
-    private TextView address2;
-    private TextView city;
-    private TextView state;
-    private TextView zip;
+    private TextView userAddress1;
+    private TextView userAddress2;
+    private TextView userCity;
+    private TextView userState;
+    private TextView userZip;
+    private TextView userOther;
 
     private Firebase myFirebaseRef;
 
     private User user;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    public ProfileActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        userName = (TextView)findViewById(R.id.profile_name);
-        userEmail = (TextView)findViewById(R.id.profile_email);
+        userName = (TextView) findViewById(R.id.profile_name);
+        userEmail = (TextView) findViewById(R.id.profile_email);
+        userPhone = (TextView) findViewById(R.id.profile_phone);
+        userAddress1 = (TextView) findViewById(R.id.profile_address1);
+        userAddress2 = (TextView) findViewById(R.id.profile_address2);
+        userCity = (TextView) findViewById(R.id.profile_city);
+        userState = (TextView) findViewById(R.id.profile_state);
+        userZip = (TextView) findViewById(R.id.profile_zip);
+        userOther = (TextView) findViewById(R.id.profile_other);
 
         Firebase.setAndroidContext(this);
         myFirebaseRef = new Firebase(getResources().getString(R.string.firebase_url));
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected void onResume() {
@@ -45,22 +68,59 @@ public class ProfileActivity extends AppCompatActivity {
         //user is logged in
         //TODO: waitForUpdate() causes black screen, but need to wait for database to update
         //TODO:     solution: turn this into an AsyncTask
-        if (userData != null)
-        {
+        if (userData != null) {
             user = new User(userData, getApplicationContext());
             //user.waitForUpdate();
             populateProfileFields();
         }
         //user is not logged in
-        else
-        {
+        else {
             //TODO: handle not logged in error?
         }
     }
 
-    private void populateProfileFields()
-    {
+    private void populateProfileFields() {
         userName.setText(user.getName());
         userEmail.setText(user.getEmail());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Profile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://sdgkteam10.rent_it/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Profile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://sdgkteam10.rent_it/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
