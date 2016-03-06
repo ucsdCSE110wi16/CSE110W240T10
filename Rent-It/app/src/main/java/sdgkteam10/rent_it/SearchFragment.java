63327bfda77c259bd132bb6720eeff6da08d29cf
David Ruble <david.ruble1@yahoo.com>
2016-03-06 04:38:52 -0800

    Added search functionality

--- Rent-It/app/src/main/java/sdgkteam10/rent_it/SearchFragment.java
+++ Rent-It/app/src/main/java/sdgkteam10/rent_it/SearchFragment.java
@@ -3,6 +3,7 @@ package sdgkteam10.rent_it;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SimpleAdapter;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.firebase.client.ChildEventListener;
@@ -20,7 +22,10 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.ui.FirebaseListAdapter;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class SearchFragment extends Fragment {
 
@@ -31,6 +36,7 @@ public class SearchFragment extends Fragment {
     private ListView listView_S;
     private SearchView searchView_S;
     private FirebaseListAdapter<Item> mAdapter;
+    private TextView emptyView;
 
     //database reference
     Database db;
@@ -55,18 +61,26 @@ public class SearchFragment extends Fragment {
         listView_S = (ListView) rootView.findViewById(R.id.listView_S);
         listView_S.setTextFilterEnabled(true);
 
+        //set default text of list view (when no results are present)
+        emptyView = new TextView(getActivity());
+        emptyView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.FILL_PARENT));
+        emptyView.setText(R.string.loading_items);
+        emptyView.setTextSize(20);
+        emptyView.setVisibility(View.GONE);
+        emptyView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+        ((ViewGroup)listView_S.getParent()).addView(emptyView);
+        listView_S.setEmptyView(emptyView);
 
         //Query queryRef = ref.child("items").orderByChild("name").endAt("mustang").limitToLast(10);
-        Query queryRef = db.getRef().child("items").limitToLast(20);
+        final Query queryRef = db.getRef().child(getActivity().getString(R.string.firebase_items)).limitToLast(20);
 
         //display the items in the query
-        //TODO: create custom layout
         mAdapter = new FirebaseListAdapter<Item>(getActivity(), Item.class, android.R.layout.two_line_list_item, queryRef) {
             @Override
             protected void populateView(View view, Item item, int i) {
+                //TODO: create custom layout and fill in all fields
                 ((TextView)view.findViewById(android.R.id.text1)).setText(item.getItemName());
                 ((TextView)view.findViewById(android.R.id.text2)).setText(item.getPrice());
-
                 //add the item to the list of searchable items
                 items.add(item);
             }
@@ -78,18 +92,68 @@ public class SearchFragment extends Fragment {
             @Override
             //called when the user submits a query
             public boolean onQueryTextSubmit(String query) {
-                //TODO: implement search functionality
+                //notify user that searching has begun
+                emptyView.setText(R.string.searching_items);
+
+                //iterate through items ArrayList and check different values of each item
+                //remove items from the listview that do not match the criteria
+                query = query.toLowerCase();
+
+                //stores the items matching the search
+                final ArrayList<Item> results = new ArrayList<Item>(0);
+
+                //search the items for matching results
+                for (Item item: items) {
+                    if (item.getItemName().toLowerCase().contains(query) ||
+                        item.getCategory().toLowerCase().contains(query) ||
+                        item.getDescription().toLowerCase().contains(query)) {
+
+                        //item matched the query, so add to results
+                        results.add(item);
+                    }
+                }
+
+                //no results found
+                if (results.size() == 0) {
+                    emptyView.setText(R.string.no_items_found);
+                }
+
+                //create a new adapter to display the results of the search
+                ArrayAdapter<Item> resultsAdapter = new ArrayAdapter<Item>(
+                        getActivity(), android.R.layout.two_line_list_item, android.R.id.text1, results) {
+                    @Override
+                    //fills in the result listView fields
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        //TODO: create custom layout and fill in all fields
+                        View view = super.getView(position, convertView, parent);
+                        TextView nameText = (TextView) view.findViewById(android.R.id.text1);
+                        TextView priceText = (TextView) view.findViewById(android.R.id.text2);
+
+                        nameText.setText(results.get(position).getItemName());
+                        priceText.setText(results.get(position).getPrice());
+                        return view;
+                    }
+                };
+                listView_S.setAdapter(resultsAdapter);
 
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                return false;
+                //if string is empty, repopulate the original listview
+                if (newText.equals("")) {
+                    listView_S.setAdapter(mAdapter);
+                    mAdapter.notifyDataSetChanged();
+                    listView_S.invalidateViews();
+                    emptyView.setText(R.string.loading_items);
+                }
+                return true;
             }
         });
 
         //TODO: when an item is clicked on, go to the view listing activity
+        //view listing activity functionality
         listView_S.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
